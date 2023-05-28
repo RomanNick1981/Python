592df3cc3655517b0638ea208d281b0a61986a5c
Roman <romaneskm@mail.ru>
2023-05-28 16:16:52 +0300

    Task 12 done

--- task12.py
+++ task12.py
@@ -1,7 +1,11 @@
 print("Петя загадал вам числа, А и Б от 1000")
 a = int(input("Вы - Катя, назовите число А "))
 b = int(input("Вы - Катя, назовите число Б "))
+flag = False
 for i in range(a):
     for j in range(b):
         if a == i + j and b == i * j:
             print(i, j)
+            flag = True
+if not flag:
+    print("Петя вас наколол")
