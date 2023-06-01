dc33d4ed414fd90500aaee46a743ee57a3bf0e08
Roman <romaneskm@mail.ru>
2023-06-01 13:37:39 +0300

    семинар5

---
+++ seminar5.py
@@ -0,0 +1,23 @@
+# def fib(n):
+#     if n == 0:
+#         return 1
+#     elif n == 1:
+#         return 1
+#     return fib(n - 1) + fib(n - 2)
+
+# n = int (input("Номер элемента ряда Фибоначчи: "))
+# print(fib(n))
+
+# Хакер Вася получил доступ 
+# def vasia(array):
+#    min_numbers = min(array)
+#    max_numbers = max(array)
+#    for i in range(len(array)):
+#       if array[i] == max_numbers:
+#          array[i] = min_numbers
+#    return array
+
+
+# n = int(input('Введите количество оценок: '))
+# list_1 = [int(i) for i in input().split()][:n]
+# print(*vasia(list_1))
